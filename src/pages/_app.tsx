6906c919de4f2dc14712db103f1ba238047bd241
Sharansrj567 <sharansrj567@gmail.com>
2023-05-30 11:46:05 +0530

    fix(*): fix title issue

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import Head from "next/head";
 import Layout from "@/components/layout";
 import "bootstrap/dist/css/bootstrap.min.css";
 // import "bootstrap/dist/js/bootstrap.min.js";
@@ -7,6 +8,18 @@ import "bootstrap/dist/css/bootstrap.min.css";
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <Layout>
+      <Head>
+        <title>KMC Genetics</title>
+        <meta
+          name="description"
+          content="Manipal Genetics Update VII
+Cellular and Animal Models
+for Rare Genetic Diseases
+18-20 January 2024"
+        />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        {/* <link rel="icon" href="/favicon.ico" /> */}
+      </Head>
       <Component {...pageProps} />
     </Layout>
   );
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,24 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <title>KMC Genetics</title>
+        <meta
+          name="description"
+          content="Manipal Genetics Update VII
+Cellular and Animal Models
+for Rare Genetic Diseases
+18-20 January 2024"
+        />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        {/* <link rel="icon" href="/favicon.ico" /> */}
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
